f98bd26cc15513cfda24d40bd1a33d47ef91293e
aubry224 <aubry224@unicaen.fr>
2023-09-25 15:37:57 +0200

    Fixed typos

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@
 // Implementation of fixed code weather request
 
 const token = "e9573bea167f06b5ca0805e4ef64e697562f702d022c084058058f958b11d272"
-const codeInsee = '22278'
+const codeInsee = '13013'
 const req = `https://api.meteo-concept.com/api/forecast/daily/0?token=${token}&insee=${codeInsee}`;
 
 fetch(req)
@@ -17,8 +17,8 @@ fetch(req)
             `Weather for ${apiWeather["city"]["name"]} (${apiWeather["city"]["latitude"]}°, ${apiWeather["city"]["longitude"]}°): <br>
                 Min temperature: ${apiWeather["forecast"]["tmin"]}°C <br>
                 Max temperature: ${apiWeather["forecast"]["tmax"]}°C <br>
-                Rain probality: ${apiWeather["forecast"]["probarain"]} % <br>
-                Accumation of rain: ${apiWeather["forecast"]["rr10"]}mm <br>
+                Rain probability: ${apiWeather["forecast"]["probarain"]} % <br>
+                Accumulation of rain: ${apiWeather["forecast"]["rr10"]}mm <br>
                 Hours of sunlight: ${apiWeather["forecast"]["sun_hours"]}h <br>
                 Average wind speed: ${apiWeather["forecast"]["wind10m"]}km/h <br>
                 Wind direction: ${apiWeather["forecast"]["dirwind10m"]}° <br><br>`
